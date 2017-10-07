1b075093db52d05225324c362a1e7424ac561291
Daniel Micay <danielmicay@gmail.com>
2017-10-07 15:56:35 -0400

    add OTHER_SENSORS permission group
    ported from 10: fc8c816e07ce39583774db8fe668e0505b6aa504
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- PermissionController/src/com/android/permissioncontroller/permission/utils/Utils.java
+++ PermissionController/src/com/android/permissioncontroller/permission/utils/Utils.java
@@ -26,6 +26,7 @@ import static android.Manifest.permission_group.MICROPHONE;
 import static android.Manifest.permission_group.NEARBY_DEVICES;
 import static android.Manifest.permission_group.NETWORK;
 import static android.Manifest.permission_group.NOTIFICATIONS;
+import static android.Manifest.permission_group.OTHER_SENSORS;
 import static android.Manifest.permission_group.PHONE;
 import static android.Manifest.permission_group.READ_MEDIA_AURAL;
 import static android.Manifest.permission_group.READ_MEDIA_VISUAL;
@@ -338,6 +339,7 @@ public final class Utils {
         PLATFORM_PERMISSIONS.put(Manifest.permission.BODY_SENSORS, SENSORS);
 
         PLATFORM_PERMISSIONS.put(Manifest.permission.INTERNET, NETWORK);
+        PLATFORM_PERMISSIONS.put(Manifest.permission.OTHER_SENSORS, OTHER_SENSORS);
 
         if (SdkLevel.isAtLeastT()) {
             PLATFORM_PERMISSIONS.put(Manifest.permission.POST_NOTIFICATIONS, NOTIFICATIONS);
